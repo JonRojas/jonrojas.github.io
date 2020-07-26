bd0cc07aa4aff1fe3b7eb301e21557a69e03edef
JonRojas <jonathan.rojas@gmail.com>
2020-07-26 19:38:19 -0400

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `Jon Rojas`,
   },
   plugins: [
     {
@@ -26,11 +26,11 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/jonrojas`,
           },
           {
-            name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/jonrojas13/`,
           },
         ],
       },
@@ -45,9 +45,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Jon Rojas`,
+        short_name: `Jon Rojas`,
+        description: `The website of Jon Rojas.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
